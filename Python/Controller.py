1ef766a6af5d46a07f646423b5eb1964e33ad2d3
Ritorta <meddesu@yandex.ru>
2023-11-04 12:51:49 +0300

    Create save / load note

--- Python/Controller.py
+++ Python/Controller.py
@@ -1,5 +1,7 @@
 def main():
     while True:
         num = view.user_input
-
-        if choice == "1":
+        if num == "1":
+            noteapp.add_note(title, body)
+        else:
+            print('Error: Invalid number, choice number is menu.')
---
+++ Python/noteapp.py
@@ -0,0 +1,22 @@
+def __init__(self):
+    self.notes = []
+    self.load_notes()
+
+def load_notes(self):
+    try:
+        with open("notes.json", "r") as file:
+            data = json.load(file)
+            for note_data in data:
+                note = Note(note_data["note_id"], note_data["title"], note_data["body"], note_data["created_time"], note_data["modified_time"])
+                self.notes.append(note)
+    except FileNotFoundError:
+        self.notes = []
+
+def save_notes(self):
+    data = []
+    for note in self.notes:
+        note_data = {"note_id": note.note_id, "title": note.title, "body": note.body, "created_time": note.created_time, "modified_time": note.modified_time}
+        data.append(note_data)
+    with open("notes.json", "w") as file:
+        json.dump(data, file, indent=4)
+
