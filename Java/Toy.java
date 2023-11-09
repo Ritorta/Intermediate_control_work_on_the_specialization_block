0b1ae0ad77fc01c4000e453b7dda0dd60480dfa0
Ritorta <meddesu@yandex.ru>
2023-11-09 17:00:19 +0300

    Add id, name, quantity, weight, dropChance, Constructor and geters

--- Java/Toy.java
+++ Java/Toy.java
@@ -1,4 +1,48 @@
 public class Toy 
 {
-    
+    private int id;
+    private String name;
+    private int quantity;
+    private int weight;
+    private int dropChance;
+
+    public Toy(int id, String name, int quantity, int weight, int dropChance) 
+    {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.weight = weight;
+        this.dropChance = dropChance;
+
+    }
+
+    public int getId() 
+    {
+        return id;
+
+    }
+
+    public String getName() 
+    {
+        return name;
+
+    }
+
+    public int getQuantity() 
+    {
+        return quantity;
+
+    }
+
+    public int getWeight() 
+    {
+        return weight;
+
+    }
+
+    public int getDropChance() 
+    {
+        return dropChance;
+
+    }
 }
