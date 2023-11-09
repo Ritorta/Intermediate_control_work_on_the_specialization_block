500b4fd6cb3e07b0cf10dea3c1701f8ec547fcd3
Ritorta <meddesu@yandex.ru>
2023-11-09 17:18:21 +0300

    create metod add PriorityQueue

--- Java/Controller.java
+++ Java/Controller.java
@@ -1,4 +1,31 @@
+import java.util.PriorityQueue;
+
 public class Controller 
 {
+    private PriorityQueue<Toy> toyQueue;
+
+    public void addToy(Toy toy) 
+    {
+        if (toy.getQuantity() != 0)
+        {
+            for (int i = 0; i < toy.getQuantity(); i++) 
+            {
+                toyQueue.offer(toy);
+            }
+        }
+        else
+        {
+            System.out.println("Toys " + toy.getName() + " ID " + toy.getId() + " are over!");
+        }
+    }
     
+    
+    
+    
+    
+    
+    
+    
+    
+   
 }
