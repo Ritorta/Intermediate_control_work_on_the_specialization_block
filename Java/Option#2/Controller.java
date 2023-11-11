60c140c3f14ceb9ef5ea6f79ce5804f8f5da5549
Ritorta <meddesu@yandex.ru>
2023-11-11 16:47:17 +0300

    create metods: Roll drop cnace, roll prize toy and list prize toy

--- Java/Option#2/Controller.java
+++ Java/Option#2/Controller.java
@@ -1,14 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Controller 
 {
     private List<Toy> toy;
+    private List<Toy> prizeToys;
 
     public Controller()
     {
         toy = new ArrayList<>();
-
+        prizeToys = new ArrayList<>();
     }
 
     public void addToy(Toy toy)
@@ -28,6 +30,52 @@ public class Controller
             }
         }
     }
+
+    private double RollDropChance()
+    {
+        double DropChance = 0;
+        for (Toy toy : toy) 
+        {
+            DropChance += toy.getDropChance();
+        }
+        return DropChance;
+    } 
+
+    public void RollPrizeToy()
+    {
+        double DropChance = RollDropChance();
+        // Random Generator
+        Random rand = new Random();
+        double randomNumber = rand.nextDouble() * DropChance;
+        // Search the prize toy
+        if (toy.isEmpty()) 
+        {
+            System.out.println("Toys are over");
+        }
+
+        if (DropChance <= 0) 
+        {
+            System.out.println("Error DropChance = 0 or -1");
+        }
+        else
+        {
+            Toy prizeToy = null;
+            for (Toy toy : toy) {
+                if (randomNumber < toy.getDropChance()) 
+                {
+                    prizeToy = toy;
+                    break;
+                }
+                randomNumber -= toy.getDropChance();
+            }
+        }
+        
+        
+    }
+
+    
+    
+        
 
 
 
